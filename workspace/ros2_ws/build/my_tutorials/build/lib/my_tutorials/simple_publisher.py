9a6fd047c237b61cc1f71dfa240b718972d4e357
sagar16812 <sagar16812@gmail.com>
2024-10-23 18:29:29 +0530

    Removed all previous packages, recreate from the beginning to match the scripts used in the tutorials

---
+++ workspace/ros2_ws/build/my_tutorials/build/lib/my_tutorials/simple_publisher.py
@@ -0,0 +1,25 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class SimplePublisher(Node):
+    def __init__(self):
+        super().__init__('simple_publisher')
+        self.publisher_ = self.create_publisher(String, 'custom_topic', 10)
+        self.timer = self.create_timer(1.0, self.publish_message)
+
+    def publish_message(self):
+        msg = String()
+        msg.data = 'Hello, this is a custom message!'
+        self.publisher_.publish(msg)
+        self.get_logger().info(f'Publishing: "{msg.data}"')
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = SimplePublisher()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
--- workspace/ros2_ws/build/rpi_robo_imu/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/sagar/Developer/Robotics/Getting-Started-with-ROS2-A-Tutorial-Series/workspace/ros2_ws/install/rpi_robo_imu'
